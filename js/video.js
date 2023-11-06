641c244bda7e8d04041df00c94fe92d3b2d0e89b
Eric Xu <xeric.2002@gmail.com>
2023-11-06 13:59:20 -0500

    Finish JavaScript

---
+++ js/video.js
@@ -0,0 +1,72 @@
+const video = document.getElementById("player1");
+video.setAttribute("preload", "auto");
+
+const playBtn = document.getElementById("play");
+const volumeSlider = document.getElementById("slider");
+const volumeLbl = document.getElementById("volume");
+// Play the video and display the volume on click
+playBtn.addEventListener("click", () => {
+  video.play();
+  volumeLbl.innerHTML = volumeSlider.value + "%";
+});
+
+const pauseBtn = document.getElementById("pause");
+// Pause the video and clear the volume information on click
+pauseBtn.addEventListener("click", () => {
+  // Pause the video and clear the volume information
+  video.pause();
+  volumeLbl.innerHTML = "";
+});
+
+const slowerBtn = document.getElementById("slower");
+// Slow down the video by 10% on click
+slowerBtn.addEventListener("click", () => {
+  // Round to 2 decimal places
+  video.playbackRate =
+    Math.round(Math.max(video.playbackRate - 0.1, 0.1) * 100) / 100;
+  console.log(video.playbackRate);
+});
+
+const fasterBtn = document.getElementById("faster");
+// Speed up the video by 10% on click
+fasterBtn.addEventListener("click", () => {
+  // Round to 2 decimal places
+  video.playbackRate = Math.round((video.playbackRate + 0.1) * 100) / 100;
+  console.log(video.playbackRate);
+});
+
+const skipBtn = document.getElementById("skip");
+// Skip ahead 10 seconds on click. Reset to start if past end of video
+skipBtn.addEventListener("click", () => {
+  const newTime = video.currentTime + 10;
+  video.currentTime = newTime > video.duration ? 0 : newTime;
+});
+
+const muteBtn = document.getElementById("mute");
+// Toggle mute on/off on click
+muteBtn.addEventListener("click", () => {
+  video.muted = !video.muted;
+  muteBtn.innerHTML = video.muted ? "Unmute" : "Mute";
+});
+
+// Update volume when slider changes
+volumeSlider.addEventListener("change", () => {
+  // Update volume label. Don't update if video is paused
+  if (video.paused) return;
+  volumeLbl.innerHTML = volumeSlider.value + "%";
+
+  // Update volume
+  video.volume = volumeSlider.value / 100;
+});
+
+const oldSchoolBtn = document.getElementById("vintage");
+// Add the oldSchool class to the video element on click
+oldSchoolBtn.addEventListener("click", () => {
+  video.classList.add("oldSchool");
+});
+
+const origBtn = document.getElementById("orig");
+// Remove the oldSchool class from the video element on click
+origBtn.addEventListener("click", () => {
+  video.classList.remove("oldSchool");
+});
